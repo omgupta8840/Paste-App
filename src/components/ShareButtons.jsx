2d7098259f8fedbcd5df3b97cc5fe12f4291df50
omgupta8840 <jayg8840@gmail.com>
2025-01-07 16:36:58 +0530

    ShareButtom Change

--- src/components/ShareButtons.jsx
+++ src/components/ShareButtons.jsx
@@ -1,26 +1,3 @@
-// import { FacebookShareButton, TwitterShareButton, LinkedinShareButton, WhatsappShareButton } from "react-share";
-
-// function ShareButtons({ text }) {
-//   return (
-//     <div className="flex space-x-4">
-//       <FacebookShareButton quote={text}>
-//         <button className="bg-blue-600 text-white px-4 py-2 rounded">Facebook</button>
-//       </FacebookShareButton>
-//       <TwitterShareButton title={text}>
-//         <button className="bg-blue-400 text-white px-4 py-2 rounded">Twitter</button>
-//       </TwitterShareButton>
-//       <WhatsappShareButton title={text}>
-//         <button className="bg-green-500 text-white px-4 py-2 rounded">
-//            WhatsApp
-//         </button>
-//       </WhatsappShareButton>
-//     </div>
-//   );
-// }
-
-// export default ShareButtons;
-
-
 
 import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from "react-share";
 
